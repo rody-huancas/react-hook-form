b3fc9aa9d8f3ce6aa04dd1eb6e0cd910e8f7e667
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-02 17:22:16 -0500

    Add: data for register

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { useForm } from "react-hook-form";
 
-export default App
+const App = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <form onSubmit={onSubmit}>
+      {/* nombre */}
+      <label htmlFor="nombre">Nombre</label>
+      <input type="text" {...register("nombre")} />
+
+      {/* correo */}
+      <label htmlFor="correo">Correo</label>
+      <input type="email" {...register("email")} />
+
+      {/* password */}
+      <label htmlFor="password">Password</label>
+      <input type="password" {...register("password")} />
+
+      {/* confirmar password */}
+      <label htmlFor="confirmarPassword">Confirmar Password</label>
+      <input type="password" {...register("confirmarPassword")} />
+
+      {/* fecha nacimiento */}
+      <label htmlFor="fechaNacimiento">Fecha Nacimiento</label>
+      <input type="date" {...register("fechaNacimiento")} />
+
+      {/* pais */}
+      <label htmlFor="pais">País</label>
+      <select {...register("pais")}>
+        <option value="mx">Mexico</option>
+        <option value="co">Colombia</option>
+        <option value="ar">Argentina</option>
+      </select>
+
+      {/* file */}
+      <label htmlFor="foto">Foto</label>
+      <input type="file" {...register("foto")} />
+
+      {/* terminos */}
+      <label htmlFor="terminos">Términos y condiciones</label>
+      <input type="checkbox" {...register("terminos")} />
+
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default App;
